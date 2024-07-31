36842ec9c353a2166d010775a65b467dffb688cd
juandanieldev29 <juandanieldev29@gmail.com>
2024-07-31 16:11:22 -0600

    removing old auth method (#19)

--- src/lib/microservice.ts
+++ src/lib/microservice.ts
@@ -14,13 +14,11 @@ interface RaffleHubMicroservicesProps {
 export class RaffleHubMicroservices extends Construct {
   public readonly raffleIndexMicroservice: NodejsFunction;
   public readonly raffleNewMicroservice: NodejsFunction;
-  public readonly signInMicroservice: NodejsFunction;
 
   constructor(scope: Construct, id: string, props: RaffleHubMicroservicesProps) {
     super(scope, id);
     this.raffleIndexMicroservice = this.createRaffleIndexFunction(props.raffleTable);
     this.raffleNewMicroservice = this.createRaffleNewFunction(props.raffleTable);
-    this.signInMicroservice = this.createSignInFunction(props.googleOAuthConfigSecret);
   }
 
   private createRaffleIndexFunction(raffleTable: ITable): NodejsFunction {
@@ -62,21 +60,6 @@ export class RaffleHubMicroservices extends Construct {
 
     raffleTable.grantReadWriteData(lambdaFunction);
 
-    return lambdaFunction;
-  }
-
-  private createSignInFunction(googleOAuthConfigSecret: ISecret): NodejsFunction {
-    const nodeJsFunctionProps: NodejsFunctionProps = {
-      bundling: {
-        externalModules: ['aws-sdk'],
-      },
-      runtime: Runtime.NODEJS_20_X,
-    };
-    const lambdaFunction = new NodejsFunction(this, 'SignInLambdaFunction', {
-      entry: join(__dirname, `/../back/auth/signin.ts`),
-      ...nodeJsFunctionProps,
-    });
-    googleOAuthConfigSecret.grantRead(lambdaFunction);
     return lambdaFunction;
   }
 }
