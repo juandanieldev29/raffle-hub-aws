1d10c12ac1d2459473e45ddd92e5db06b1ca874c
juandanieldev29 <juandanieldev29@gmail.com>
2024-07-31 09:11:21 -0600

    fetching user attributes

--- src/front/src/components/avatar.tsx
+++ src/front/src/components/avatar.tsx
@@ -1,14 +1,26 @@
 'use client';
 
 import { useAuthenticator } from '@aws-amplify/ui-react';
+import { fetchUserAttributes } from 'aws-amplify/auth';
+import { useEffect } from 'react';
 
 export default function Avatar() {
-  const { user, route } = useAuthenticator((context) => [context.user, context.route]);
+  const { user } = useAuthenticator((context) => [context.user]);
+
+  const fetchCurrentUser = async () => {
+    const user = await fetchUserAttributes();
+    console.log(user);
+  };
+
+  useEffect(() => {
+    if (user) {
+      fetchCurrentUser;
+    }
+  }, [user]);
 
   return (
     <>
       <h1>{user ? user.username : 'User not logged in'}</h1>
-      <p>Current route {route}</p>
     </>
   );
 }
