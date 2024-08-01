bf8f47e56c5fa992180a05c94fab14195d2cd598
juandanieldev29 <juandanieldev29@gmail.com>
2024-07-31 19:19:19 -0600

    inserting owner when creating raffle (#22)

--- src/back/raffle/new.ts
+++ src/back/raffle/new.ts
@@ -1,4 +1,4 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyWithCognitoAuthorizerEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { PutItemCommand } from '@aws-sdk/client-dynamodb';
 import { marshall } from '@aws-sdk/util-dynamodb';
 import { v4 as uuidv4 } from 'uuid';
@@ -13,8 +13,34 @@ interface CreateRaffleBody {
   description: string;
 }
 
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+interface CognitoUserSession {
+  sub: string;
+  email: string;
+  given_name: string;
+  family_name?: string;
+  picture?: string;
+}
+
+interface Owner {
+  id: string;
+  email: string;
+  name: string;
+  photoURL?: string;
+}
+
+export const handler = async (
+  event: APIGatewayProxyWithCognitoAuthorizerEvent,
+): Promise<APIGatewayProxyResult> => {
   try {
+    const userSession: CognitoUserSession = event.requestContext.authorizer.claims as {
+      [x in keyof CognitoUserSession]: string;
+    };
+    const owner: Owner = {
+      id: userSession.sub,
+      email: userSession.email,
+      name: formatName(userSession.given_name, userSession.family_name),
+      photoURL: userSession.picture,
+    };
     const body: CreateRaffleBody = JSON.parse(event.body!);
     const { prize, quantityNumbers = 100, quantitySeries = null, ticketPrice, description } = body;
     const id = uuidv4();
@@ -25,6 +51,10 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
       quantitySeries,
       ticketPrice,
       description,
+      boughtTickets: 0,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+      owner,
     };
     const params = {
       TableName: process.env.DYNAMODB_TABLE_NAME,
@@ -48,4 +78,11 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
       body: JSON.stringify('some error happened'),
     };
   }
+};
+
+const formatName = (givenName: string, familyName: string | undefined) => {
+  if (!familyName) {
+    return givenName;
+  }
+  return `${givenName} ${familyName}`;
 };
