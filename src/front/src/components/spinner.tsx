2aa7ec80e1e818bf24828c5cd14d589ef5fc2ccb
juandanieldev29 <juandanieldev29@gmail.com>
2024-08-14 15:33:03 -0600

    using tailwind to define spinner styles

--- src/front/src/components/spinner.tsx
+++ src/front/src/components/spinner.tsx
@@ -8,8 +8,11 @@ export default function Spinner() {
   if (state.httpRequestsCount <= 0) return null;
 
   return (
-    <div className="spinner">
-      <div className="spinner-border" role="status" />
+    <div className="flex justify-center items-center fixed top-0 left-0 w-full h-full z-50 bg-black/50">
+      <div
+        className="w-12 h-12 border-4 rounded-full border-black/50 border-t-blue-600 animate-spin"
+        role="status"
+      />
     </div>
   );
 }
