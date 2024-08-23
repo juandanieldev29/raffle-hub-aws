70db9d7fc295fe5097900fb96bd6ce2645b96edf
juandanieldev29 <juandanieldev29@gmail.com>
2024-08-23 14:43:50 -0600

    updating tickets to complete or expired if any

--- src/back/payment/success.ts
+++ src/back/payment/success.ts
@@ -19,8 +19,10 @@ export const handler: SQSHandler = async (event: SQSEvent): Promise<void> => {
   for (const message of event.Records) {
     const expireTicketEventRequest: IPaymentSuccessPayloadBody = JSON.parse(message.body);
     const messageDetail = expireTicketEventRequest.detail;
-    const ticketsId = await getTicketByRaffleAndPayment(messageDetail);
-    await setTicketsStatusToComplete(ticketsId);
+    const tickets = await getTicketByRaffleAndPayment(messageDetail);
+    if (tickets.length) {
+      await setTicketsStatusToComplete(tickets);
+    }
   }
 };
 
