d002a81559bd8e75cc89894c2a2ef31b1e1849c5
juandanieldev29 <juandanieldev29@gmail.com>
2024-08-29 11:10:57 -0600

    adding logs to create new payment (#66)
    * adding logs to create new payment
    
    * accepting id tokens

--- src/back/payment/new.ts
+++ src/back/payment/new.ts
@@ -105,14 +105,15 @@ const verifyToken = async (
   }
   const verifier = CognitoJwtVerifier.create({
     userPoolId: process.env.USER_POOL_ID,
-    tokenUse: 'access',
+    tokenUse: 'id',
     clientId: process.env.USER_POOL_CLIENT_ID,
   });
   try {
     const token = extractToken(authorizationHeader);
     const payload = await verifier.verify(token);
     return { id: payload.sub };
-  } catch {
+  } catch (err) {
+    console.log(err);
     console.log('Access token not valid');
   }
   return null;
