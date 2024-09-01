ea85df1085dce81f6cd756e45ada57d8bcfa4208
juandanieldev29 <juandanieldev29@gmail.com>
2024-09-01 13:46:08 -0600

    adding configuration to call cognito auth from server side

---
+++ src/front/src/app/raffle/page.tsx
@@ -0,0 +1,24 @@
+import { cookies } from 'next/headers';
+import { fetchAuthSession } from 'aws-amplify/auth/server';
+import { runWithAmplifyServerContext } from '@/utils/amplifyServerUtils';
+
+export const dynamic = 'force-dynamic';
+
+export default async function MyRaffle() {
+  try {
+    const session = await runWithAmplifyServerContext({
+      nextServerContext: { cookies },
+      operation: (contextSpec) => fetchAuthSession(contextSpec),
+    });
+
+    return (
+      <>
+        <h1>Hola {session.userSub}</h1>
+        <p>User pool id: {process.env.USER_POOL_ID}</p>
+      </>
+    );
+  } catch (error) {
+    console.error(error);
+    return <p>Something went wrong...</p>;
+  }
+}
