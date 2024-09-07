e8a9499bf431bb2b6ffffa89b15dee09bcfcccda
juandanieldev29 <juandanieldev29@gmail.com>
2024-09-06 21:18:30 -0600

    adding error logging to main page (#90)

--- src/front/src/app/page.tsx
+++ src/front/src/app/page.tsx
@@ -5,15 +5,19 @@ import { INITIAL_LIMIT } from '@/utils/constants';
 import { RafflesPaginationResult } from '@/types/pagination';
 
 export default async function Home() {
-  const res = await fetch(`https://api.raffle-hub.net/raffle?limit=${INITIAL_LIMIT}`, {
-    headers: headers(),
-    cache: 'no-store',
-  });
-  const rafflesPaginated: RafflesPaginationResult = await res.json();
+  try {
+    const res = await fetch(`https://api.raffle-hub.net/raffle?limit=${INITIAL_LIMIT}`, {
+      headers: headers(),
+      cache: 'no-store',
+    });
+    const rafflesPaginated: RafflesPaginationResult = await res.json();
 
-  return (
-    <main className="margin-top margin-bottom">
-      <RaffleList rafflesPaginated={rafflesPaginated} />
-    </main>
-  );
+    return (
+      <main className="margin-top margin-bottom">
+        <RaffleList rafflesPaginated={rafflesPaginated} />
+      </main>
+    );
+  } catch (err: any) {
+    <p>An error happened {err.message}</p>;
+  }
 }
