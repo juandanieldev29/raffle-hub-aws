28d05baf3131e599dce39ab5a18d6c7a1bbad0d0
juandanieldev29 <juandanieldev29@gmail.com>
2024-09-06 21:40:57 -0600

    removing headers from server side request (#93)

--- src/front/src/app/page.tsx
+++ src/front/src/app/page.tsx
@@ -1,20 +1,17 @@
-import { headers } from 'next/headers';
-
 import RaffleList from '@/components/pages/raffle';
 import { INITIAL_LIMIT } from '@/utils/constants';
 import { RafflesPaginationResult } from '@/types/pagination';
 
 export default async function Home() {
   try {
-    // const res = await fetch(`https://api.raffle-hub.net/raffle?limit=${INITIAL_LIMIT}`, {
-    //   headers: headers(),
-    //   cache: 'no-store',
-    // });
-    // const rafflesPaginated: RafflesPaginationResult = await res.json();
+    const res = await fetch(`https://api.raffle-hub.net/raffle?limit=${INITIAL_LIMIT}`, {
+      cache: 'no-store',
+    });
+    const rafflesPaginated: RafflesPaginationResult = await res.json();
 
     return (
       <main className="margin-top margin-bottom">
-        <RaffleList rafflesPaginated={{ raffles: [], lastEvaluatedKey: null }} />
+        <RaffleList rafflesPaginated={rafflesPaginated} />
       </main>
     );
   } catch (err: any) {
