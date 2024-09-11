90c7437d56622464fde287548677cdfae3252b27
juandanieldev29 <juandanieldev29@gmail.com>
2024-09-10 18:39:17 -0600

    sending authentication in server side rendering

--- src/front/src/app/raffle/[raffleId]/billing/page.tsx
+++ src/front/src/app/raffle/[raffleId]/billing/page.tsx
@@ -1,25 +1,37 @@
+import { cookies } from 'next/headers';
+import Error from 'next/error';
+import { fetchAuthSession } from 'aws-amplify/auth/server';
+
 import RaffleBilling from '@/components/pages/raffle/billing';
+import { runWithAmplifyServerContext } from '@/utils/amplifyServerUtils';
 import { IPayment, IVoucher } from '@/types';
 
 export default async function RaffleBillingPage({ params }: { params: { raffleId: string } }) {
   const { raffleId } = params;
 
-  const [paymentsRes, vouchersRes] = await Promise.all([
-    fetch(`https://api.raffle-hub.net/raffle/${raffleId}/payments`, {
-      cache: 'no-store',
-    }),
-    fetch(`https://api.raffle-hub.net/raffle/${raffleId}/vouchers`, {
-      cache: 'no-store',
-    }),
-  ]);
-  const [payments, vouchers]: [IPayment[], IVoucher[]] = await Promise.all([
-    paymentsRes.json(),
-    vouchersRes.json(),
-  ]);
+  const session = await runWithAmplifyServerContext({
+    nextServerContext: { cookies },
+    operation: (contextSpec) => fetchAuthSession(contextSpec),
+  });
+  if (session.tokens?.idToken) {
+    const [paymentsRes, vouchersRes] = await Promise.all([
+      fetch(`https://api.raffle-hub.net/raffle/${raffleId}/payments`, {
+        cache: 'no-store',
+      }),
+      fetch(`https://api.raffle-hub.net/raffle/${raffleId}/vouchers`, {
+        cache: 'no-store',
+      }),
+    ]);
+    const [payments, vouchers]: [IPayment[], IVoucher[]] = await Promise.all([
+      paymentsRes.json(),
+      vouchersRes.json(),
+    ]);
+    return (
+      <main className="margin-top margin-bottom">
+        <RaffleBilling payments={payments} vouchers={vouchers} />
+      </main>
+    );
+  }
 
-  return (
-    <main className="margin-top margin-bottom">
-      <RaffleBilling payments={payments} vouchers={vouchers} />
-    </main>
-  );
+  return <Error statusCode={401} />;
 }
