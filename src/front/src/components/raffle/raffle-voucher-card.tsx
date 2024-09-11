1f3d200293dd22d3a9f27641533bb0ae6d091d51
juandanieldev29 <juandanieldev29@gmail.com>
2024-09-10 20:04:57 -0600

    fixing voucher mobile layout (#113)

--- src/front/src/components/raffle/raffle-voucher-card.tsx
+++ src/front/src/components/raffle/raffle-voucher-card.tsx
@@ -24,7 +24,7 @@ export default function RaffleVoucherCard({
   return (
     <div
       className={classNames(
-        'width secondary-background-color padding margin-bottom rounded-2xl transition-transform grid grid-cols-1 md:grid-cols-3 grid-rows-6 md:gap-x-8 gap-2 shadow-lg min-h-12',
+        'width secondary-background-color padding margin-bottom rounded-2xl transition-transform grid grid-cols-1 md:grid-cols-3 grid-rows-4 md:grid-rows-6 md:gap-x-8 gap-2 shadow-lg min-h-12',
         {
           'md:hover:scale-[1.01]': !disableAnimations,
         },
@@ -35,7 +35,7 @@ export default function RaffleVoucherCard({
         {createdAt}
       </p>
       <p
-        className="text-sm font-extralight font-medium md:col-start-1 row-end-4 row-start-1 md:row-span-full md:col-span-2"
+        className="text-sm font-extralight font-medium md:col-start-1 row-end-5 md:row-end-4 row-start-1 md:row-span-full md:col-span-2"
         suppressHydrationWarning
       >
         <StorageImage path={`public/${voucher.url}`} alt="Factura de compra" />
