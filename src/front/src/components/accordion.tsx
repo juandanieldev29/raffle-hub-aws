f60d6f3863e55e8811e3704beb73b9a4b4321c1b
juandanieldev29 <juandanieldev29@gmail.com>
2024-09-12 11:02:33 -0600

    adding accordions to billing sections (#121)

---
+++ src/front/src/components/accordion.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { ReactNode, useState } from 'react';
+import classNames from 'classnames';
+
+interface AccordionProps {
+  containerClassname?: string;
+  title: string;
+  titleClassname?: string;
+  initiallyOpen?: boolean;
+  children: ReactNode;
+}
+
+export default function Accordion({
+  containerClassname,
+  title,
+  titleClassname,
+  initiallyOpen,
+  children,
+}: AccordionProps) {
+  const [open, setOpen] = useState(initiallyOpen ?? false);
+
+  return (
+    <div className={classNames('margin-bottom', containerClassname)}>
+      <div className="flex margin-bottom items-center w-full border-b border-b-slate-200 dark:border-b-slate-50">
+        <button
+          className={classNames('text-left small-margin-right small-margin-bottom', titleClassname)}
+          onClick={() => setOpen(!open)}
+        >
+          {title}
+        </button>
+        <span className="small-margin-bottom">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            className={classNames('w-5 h-5 transition-all', {
+              'rotate-180': open,
+              'rotate-0': !open,
+            })}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              fillRule="evenodd"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+              clipRule="evenodd"
+            />
+          </svg>
+        </span>
+      </div>
+      {open && <div>{children}</div>}
+    </div>
+  );
+}
--- src/front/src/components/pages/raffle/billing.tsx
+++ src/front/src/components/pages/raffle/billing.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { Authenticator } from '@aws-amplify/ui-react';
 import RafflePaymentCard from '@/components/raffle/raffle-payment-card';
 import RaffleVoucherCard from '@/components/raffle/raffle-voucher-card';
+import Accordion from '@/components/accordion';
 import { IPaymentStatus, IVoucherStatus } from '@/types';
 import { IVoucher, IPayment } from '@/types';
 
@@ -28,42 +29,54 @@ export default function RaffleBilling({ vouchers, payments }: RaffleBillingProps
   return (
     <Authenticator socialProviders={['google']} signUpAttributes={['email']}>
       <h1 className="width margin-bottom text-4xl md:text-5xl">Detalles de facturación</h1>
-      <h2 className="width margin-bottom text-2xl md:text-3xl font-medium">
-        Pagos completados con tarjeta
-      </h2>
-      {!Boolean(completedPayments.length) && (
-        <div className="width secondary-background-color padding margin-bottom rounded-2xl transition-transform gap shadow-lg flex items-center">
-          <i className="fa-solid fa-circle-info" />
-          <span>No hay datos</span>
-        </div>
-      )}
-      {completedPayments.map((x) => {
-        return <RafflePaymentCard key={x.id} payment={x} />;
-      })}
-      <h2 className="width margin-bottom text-2xl md:text-3xl font-medium">
-        Pagos completados con SINPE movil completados
-      </h2>
-      {!Boolean(completedVouchers.length) && (
-        <div className="width secondary-background-color padding margin-bottom rounded-2xl transition-transform gap shadow-lg flex items-center">
-          <i className="fa-solid fa-circle-info" />
-          <span>No hay datos</span>
-        </div>
-      )}
-      {completedVouchers.map((x) => {
-        return <RaffleVoucherCard key={x.id} voucher={x} />;
-      })}
-      <h2 className="width margin-bottom text-2xl md:text-3xl font-medium">
-        Pagos con SINPE movil pendientes de verificación
-      </h2>
-      {!Boolean(pendingVouchers.length) && (
-        <div className="width secondary-background-color padding margin-bottom rounded-2xl transition-transform gap shadow-lg flex items-center">
-          <i className="fa-solid fa-circle-info" />
-          <span>No hay datos</span>
-        </div>
-      )}
-      {pendingVouchers.map((x) => {
-        return <RaffleVoucherCard key={x.id} voucher={x} />;
-      })}
+      <Accordion
+        containerClassname="width"
+        titleClassname="text-2xl md:text-3xl font-medium"
+        title="Pagos con SINPE movil pendientes de verificación"
+        initiallyOpen
+      >
+        {!Boolean(pendingVouchers.length) && (
+          <div className="secondary-background-color padding margin-bottom rounded-2xl transition-transform gap shadow-lg flex items-center">
+            <i className="fa-solid fa-circle-info" />
+            <span>No hay datos</span>
+          </div>
+        )}
+        {pendingVouchers.map((x) => {
+          return <RaffleVoucherCard key={x.id} voucher={x} />;
+        })}
+      </Accordion>
+
+      <Accordion
+        containerClassname="width"
+        titleClassname="text-2xl md:text-3xl font-medium"
+        title="Pagos completados con tarjeta"
+      >
+        {!Boolean(completedPayments.length) && (
+          <div className="secondary-background-color padding margin-bottom rounded-2xl transition-transform gap shadow-lg flex items-center">
+            <i className="fa-solid fa-circle-info" />
+            <span>No hay datos</span>
+          </div>
+        )}
+        {completedPayments.map((x) => {
+          return <RafflePaymentCard key={x.id} payment={x} />;
+        })}
+      </Accordion>
+
+      <Accordion
+        containerClassname="width"
+        titleClassname="text-2xl md:text-3xl font-medium"
+        title="Pagos completados con SINPE movil completados"
+      >
+        {!Boolean(completedVouchers.length) && (
+          <div className="secondary-background-color padding margin-bottom rounded-2xl transition-transform gap shadow-lg flex items-center">
+            <i className="fa-solid fa-circle-info" />
+            <span>No hay datos</span>
+          </div>
+        )}
+        {completedVouchers.map((x) => {
+          return <RaffleVoucherCard key={x.id} voucher={x} />;
+        })}
+      </Accordion>
     </Authenticator>
   );
 }
